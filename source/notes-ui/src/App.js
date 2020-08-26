ef467dce76246607cb986e888bc92ecf3e95ab64
santheepkumar <developer.santheep@gmail.com>
2020-08-26 22:24:45 +0530

    tailwind added and configured

--- source/notes-ui/src/App.js
+++ source/notes-ui/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className='App'>
-      <h1 className='doc'>Cool Notes App</h1>
+      <h1 className='flex justify-center'>Cool Notes App</h1>
     </div>
   );
 }
