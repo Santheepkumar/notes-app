290457efadaef7230c2b64029a38f7666449893e
Santheep kumar <svsantheep@gmail.com>
2020-08-29 18:25:30 +0530

    service worker moved

--- source/notes-ui/src/components/notes/NotesController.js
+++ source/notes-ui/src/components/notes/NotesController.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-const apiUrl = "http://localhost:5000/api";
 function NotesController() {
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get(`${apiUrl}/notes`).then((res) => {
+    axios.get(`/api/notes`).then((res) => {
       const notes = res.data;
       setData(notes);
     });
   }, []);
-  const [data, setData] = useState([]);
   console.log("apiData", data);
 
   return <div>{data}</div>;
