863f56f2e593c3a673fa35f8d4669b3eebf6b6c3
santheepkumar <developer.santheep@gmail.com>
2020-08-31 19:41:28 +0530

    deployment app

--- Notes API & UI Normal/notes-api/api.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const notesRoutes = require("./routes/notes.route");
-
-router.get("/start", (req, res) => res.send("Hello Notes!!"));
-router.use("/notes", notesRoutes);
-
-module.exports = router;
