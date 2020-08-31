7a044d2f7d54002f1bd70bc7204f5cc7044a2f80
santheepkumar <developer.santheep@gmail.com>
2020-08-31 20:32:54 +0530

    heroku push

--- notes-app/index.js
+++ notes-app/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 const mongoUrl = process.env.MONGO_URL;
 mongoose.connect(mongoUrl, {
@@ -20,6 +21,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", () => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT;
 app.listen(port, () => {
